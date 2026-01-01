056ebb4919abd730ef5f3512d664fda875948910
shalebao <shalebao3@gmail.com>
2026-01-01 23:51:16 +0800

    ``` feat(project): 集成FreeRTOS到平衡车项目
    - 将编译优化级别从1提升到2
    - 更新C语言标准版本配置参数
    - 添加STM32F10X_MD预定义宏
    - 增加FreeRTOS相关头文件路径
    - 在项目中添加FreeRTOS源文件分组，包括核心源文件、
      源文件和可移植层文件
    - 在main.c中添加FreeRTOS头文件包含
    - 实现SysTick中断处理函数以支持FreeRTOS调度器
    ```

--- project/BalancingCar/USer/main.c
+++ project/BalancingCar/USer/main.c
@@ -1,9 +1,19 @@
 #include "main.h"
-
+#include "freertos.h"
+#include "task.h"
 
 int main(void)
 {
 		
 	
 	
+}
+
+extern void xPortSysTickHandler(void);
+void SysTick_Handler(void)
+{
+	if (xTaskGetSchedulerState() != taskSCHEDULER_NOT_STARTED)
+	{
+		xPortSysTickHandler();
+	}	
 }
